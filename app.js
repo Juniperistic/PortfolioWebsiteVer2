3fdbdac808f46c6b0b58d2c018a08fcff68c2e6e
Miranda Morris <juniperistic@gmail.com>
2024-01-17 13:54:47 -0500

    Testing Commit from Local Repo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Allows the Sidebar Menu used for accessing the different pages to be accessed & used
+
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
